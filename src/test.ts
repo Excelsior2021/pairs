977ef590147e34f277d60551c12cf4c32b4a4742
Jonathan Kila <temidee11@yahoo.co.uk>
2024-01-17 20:28:23 +0000

    pendo

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,3 @@
-import type { Component } from "solid-js"
-import GameScreen from "./components/GameScreen/GameScreen"
-import "./App.scss"
-
 pendo.initialize({
   visitor: {
     id: null, // Required if user is logged in
@@ -26,24 +22,3 @@ pendo.initialize({
     // as long as it's not one of the above reserved names.
   },
 })
-
-const App: Component = () => (
-  <div class="app">
-    <a
-      class="app__link"
-      href="https://jonathankila.vercel.app"
-      target="_blank"
-      rel="noreferrer">
-      <h1 class="app__title">Pairs</h1>
-    </a>
-    <a class="counter" href="https://www.free-website-hit-counter.com">
-      <img
-        src="https://www.free-website-hit-counter.com/c.php?d=9&id=143227&s=5"
-        alt="Free Website Hit Counter"
-      />
-    </a>
-    <GameScreen />
-  </div>
-)
-
-export default App
